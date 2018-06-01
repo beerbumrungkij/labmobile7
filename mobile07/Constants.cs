cfe7c6a2f788d9410f8e2ec151d551053292b746
chatchai bumrungkij <beer.bumrungkij@gmail.com>
2018-06-01 04:20:16 +0000

    Add Source code

---
+++ mobile07/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace mobile07
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://mobile07.azurewebsites.net";
+	}
+}
+
